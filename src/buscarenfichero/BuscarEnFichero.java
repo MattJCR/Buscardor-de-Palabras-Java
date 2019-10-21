a6385d2fa689f06caa5f1616ec00cc36f8426ae4
Mateo Jesus Cabello Rodriguez <44649243+MattJCR@users.noreply.github.com>
2019-10-21 08:00:10 +0200

    Add files via upload
    Añadido escritura fichero.

--- src/buscarenfichero/BuscarEnFichero.java
+++ src/buscarenfichero/BuscarEnFichero.java
@@ -8,7 +8,9 @@ package buscarenfichero;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,29 +29,33 @@ public class BuscarEnFichero {
             System.out.println("No has pasado el fichero o las palabras a buscar.");
         }else{
             try {
-                
                 int aux = 0;
                 for (int i = 1; i < args.length; i++) {
                     System.out.println("____________________________________________________________");
                     aux = 0;
+                    FileWriter fichero = null;
+                    PrintWriter pw = null;
                     BufferedReader fbr = new BufferedReader(new FileReader(args[0]));
                     String linea = fbr.readLine();
+                    fichero = new FileWriter("./Prueba.txt");
+                    pw = new PrintWriter(fichero);      
                     while(linea != null){
                         if (linea.indexOf(args[i]) > -1) {
                             System.out.println(linea);
+                            pw.println("La palabra " + args[i] + " esta en la linea: " + aux
+                                + " Columna: " + linea.indexOf(args[i]));
                             System.out.println("La palabra " + args[i] + " esta en la linea: " + aux
                                 + " Columna: " + linea.indexOf(args[i]));
                         }
                         linea = fbr.readLine();
                         aux++;
                     }
+                    fichero.close();
                 }
             } catch (FileNotFoundException ex) {
                 Logger.getLogger(BuscarEnFichero.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
-        
     }
     
 }
